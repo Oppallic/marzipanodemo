522a3ec392c24a8146c561c5b24075611490138f
Oppallic <69237331+Oppallic@users.noreply.github.com>
2021-08-17 16:45:00 +1000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,113 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pic-1",
+      "name": "pic 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -2.066983349154377,
+        "pitch": 0.06568776199646287,
+        "fov": 1.4013389225909472
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.116700030621324,
+          "pitch": 0.20417692363391993,
+          "rotation": 0,
+          "target": "1-pic-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1685987784754204,
+          "pitch": 0.2500306934651668,
+          "title": "Rohan with Cam",
+          "text": "Here Rohan is posing with a camera"
+        },
+        {
+          "yaw": 1.6291450231221791,
+          "pitch": 0.01922212187406558,
+          "title": "Nicholas with cam 2",
+          "text": "Here Nicholas is holding another camera"
+        },
+        {
+          "yaw": 0.7621766974426905,
+          "pitch": 0.02407502159185171,
+          "title": "Angel on laptop",
+          "text": "Here Angel is on her laptop"
+        }
+      ]
+    },
+    {
+      "id": "1-pic-2",
+      "name": "pic 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 1.0995574287564196,
+        "pitch": 0.1345035126593963,
+        "fov": 1.4013389225909472
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1389415795479145,
+          "pitch": 0.39194125511378886,
+          "rotation": 0,
+          "target": "0-pic-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0400687358832812,
+          "pitch": -0.14657417917079485,
+          "title": "View 2",
+          "text": "You have changed scenes"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
